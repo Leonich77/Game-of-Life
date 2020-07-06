d7def6e0efc2b859b0a4459d55ca18e927f6ecba
Leonich77 <leonich777@rambler.ru>
2020-07-06 15:12:58 +0500

    test commit

--- Game of Life/task/src/life/GameOfLife.java
+++ Game of Life/task/src/life/GameOfLife.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ItemEvent;
 
+//fghjk
 public class GameOfLife extends JFrame {
     private final JPanel livePanel;
     private char[][] currentGeneration;
